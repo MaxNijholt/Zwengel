d46c8bddbbb55fcc0a86217c384b33be5380d914
BramHendriksDeveloper <bram5688@gmail.com>
2016-03-22 14:37:33 +0100

    order en binding app

--- ZwengelHybrid/www/js/index.js
+++ ZwengelHybrid/www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        angular.bootstrap(document, ['zwengelApp']);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
