7b04e01ca50522ca942b56ea07b2cb40df2e4436
BramHendriksDeveloper <bram5688@gmail.com>
2016-03-22 14:25:47 +0100

    design + price_in_reward

--- ZwengelHybrid/www/js/DataServices.js
+++ ZwengelHybrid/www/js/DataServices.js
@@ -357,18 +357,23 @@ dataCaller.getStap = function(studentID, doelID, stapID, onSucces){
 dataCaller.getBeloningen = function(studentID, onSucces){
     var testResult = [{ 
         "title": "10 min. spelletje spelen in de les",
+        "price": 5,
         "behaald": true
     },{ 
         "title": "15 min. spelletje spelen in de les",
+        "price": 7,
         "behaald": false
     },{ 
         "title": "Thema Ridder",
+        "price": 25,
         "behaald": false
     },{ 
         "title": "Thema Koning",
+        "price": 25,
         "behaald": true
     },{ 
         "title": "Als eerste naar huis",
+        "price": 10,
         "behaald": false
     }
     ];
