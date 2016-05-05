d8f054b22bce6c6fa0e480d230f7bbaf91dc01a9
JoostVermeulen11 <jwaverme@avans.nl>
2016-05-05 09:27:57 +0200

    login nog niet werkend

--- www/js/ZwengelApp.js
+++ www/js/ZwengelApp.js
@@ -7,30 +7,57 @@ var zwengelApp = angular.module('zwengelApp', [
 ]);
 
 zwengelApp.config(['$routeProvider',
-    function($routeProvider) {
+    function ($routeProvider) {
         $routeProvider.
-        when('/doelen', {
-        	templateUrl: 'views/doelen.html',
-        	controller: 'DoelenController as denc'
-        }).
-        when('/doelen/:doelID', {
-        	templateUrl: 'views/doel.html',
-        	controller: 'DoelController as dc'
-        }).
-        when('/doelen/:doelID/:stapID', {
-        	templateUrl: 'views/stap.html',
-        	controller: 'StapController as sc'
-        }).
-        when('/beloningen', {
-        	templateUrl: 'views/beloningen.html',
-        	controller: 'BeloningenController as benc'
-        }).
-        when('/profiel', {
-        	templateUrl: 'views/profiel.html',
-        	controller: 'ProfielController as pc'
-        }).
-        otherwise({
-        	redirectTo: '/doelen'
-        });
+            // when('/doelen', {
+            // 	templateUrl: 'views/doelen.html',
+            // 	controller: 'DoelenController as denc'
+            // }).
+            // when('/doelen/:doelID', {
+            // 	templateUrl: 'views/doel.html',
+            // 	controller: 'DoelController as dc'
+            // }).
+            // when('/doelen/:doelID/:stapID', {
+            // 	templateUrl: 'views/stap.html',
+            // 	controller: 'StapController as sc'
+            // }).
+            // when('/beloningen', {
+            // 	templateUrl: 'views/beloningen.html',
+            // 	controller: 'BeloningenController as benc'
+            // }).
+            // when('/profiel', {
+            // 	templateUrl: 'views/profiel.html',
+            // 	controller: 'ProfielController as pc'
+            // }).
+            // otherwise({
+            // 	redirectTo: '/doelen'
+            // });
+            when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginController as lc'
+            }).
+            when('/doelen', {
+                templateUrl: 'views/doelen.html',
+                controller: 'DoelenController as denc'
+            }).
+            when('/doelen/:doelID', {
+                templateUrl: 'views/doel.html',
+                controller: 'DoelController as dc'
+            }).
+            when('/doelen/:doelID/:stapID', {
+                templateUrl: 'views/stap.html',
+                controller: 'StapController as sc'
+            }).
+            when('/beloningen', {
+                templateUrl: 'views/beloningen.html',
+                controller: 'BeloningenController as benc'
+            }).
+            when('/profiel', {
+                templateUrl: 'views/profiel.html',
+                controller: 'ProfielController as pc'
+            }).
+            otherwise({
+                redirectTo: '/login'
+            });
     }
 ]);
--- www/js/ZwengelControllers.js
+++ www/js/ZwengelControllers.js
@@ -1,40 +1,51 @@
 var zwengelControllers = angular.module('zwengelControllers', []);
 var controllers = {};
 
-controllers.AllController = function($ionicPlatform, $window, AllData) {
+controllers.AllController = function ($ionicPlatform, $window, AllData) {
     var self = this;
-    
+
     self.page = AllData.page;
-    
-    self.navDoelen = function(){
-        AllData.toPage("Doelen", "#/doelen");
+
+    self.navDoelen = function () {
+        AllData.toPage("Login", "#/login");
     };
-    
-    self.navBeloningen = function(){
+
+    self.navBeloningen = function () {
         AllData.toPage("Beloningen", "#/beloningen");
     };
-    
-    self.navProfiel = function(){
+
+    self.navProfiel = function () {
         AllData.toPage("Profiel", "#/profiel");
     };
-    
-    self.back = function(){
-		AllData.toBack();
+
+    self.back = function () {
+        AllData.toBack();
     };
-    
+
     $ionicPlatform.registerBackButtonAction(function () {
-		AllData.toBack(true);
-	}, 100);
+        AllData.toBack(true);
+    }, 100);
 };
 
-controllers.DoelenController = function($ionicScrollDelegate, AllData, StudentInfo) {
+controllers.LoginController = function ($ionicScrollDelegate, AllData) {
+    console.log("logincontroller reached");
     var self = this;
     
     $ionicScrollDelegate.scrollTop();
-    
-    StudentInfo.getDoelen("test", function(results){
-        results.forEach(function(object){
-            switch(object.done) {
+
+    self.doelscreen = AllData.pref.doelscreen;
+    self.textIcons = AllData.pref.textIcons;
+    self.thema = "default";
+};
+
+controllers.DoelenController = function ($ionicScrollDelegate, AllData, StudentInfo) {
+    var self = this;
+
+    $ionicScrollDelegate.scrollTop();
+
+    StudentInfo.getDoelen("test", function (results) {
+        results.forEach(function (object) {
+            switch (object.done) {
                 case "good":
                     object.doneColor = "balanced";
                     break;
@@ -47,60 +58,60 @@ controllers.DoelenController = function($ionicScrollDelegate, AllData, StudentIn
         });
         self.targets = results;
     });
-    
-    self.toDoel = function(doelID){
+
+    self.toDoel = function (doelID) {
         AllData.toPage("Doel", "#/doelen/" + doelID);
     };
 };
 
-controllers.BeloningenController = function($ionicScrollDelegate, StudentInfo) {
+controllers.BeloningenController = function ($ionicScrollDelegate, StudentInfo) {
     var self = this;
-    
+
     $ionicScrollDelegate.scrollTop();
-    
+
     self.hideFilter = "ng-hide";
-	self.filterIcon = "ion-chevron-down";
-    StudentInfo.getBeloningen("test" , function(results){
+    self.filterIcon = "ion-chevron-down";
+    StudentInfo.getBeloningen("test", function (results) {
         self.rewards = results;
     });
-    	
-    self.buyReward = function(reward){
+
+    self.buyReward = function (reward) {
         var confirm = window.confirm("Koop beloning: " + reward.title);
         if (confirm == true) {
             alert("Gekocht");
         }
     };
-        
-	self.toggleHideFilter = function(bool){
-		if(self.hideFilter === "ng-hide" && !bool){
-			self.hideFilter = "ng-show";
-			self.filterIcon = "ion-chevron-up";
-		}else{
-			self.hideFilter = "ng-hide";
-			self.filterIcon = "ion-chevron-down";
-		}
-	};
+
+    self.toggleHideFilter = function (bool) {
+        if (self.hideFilter === "ng-hide" && !bool) {
+            self.hideFilter = "ng-show";
+            self.filterIcon = "ion-chevron-up";
+        } else {
+            self.hideFilter = "ng-hide";
+            self.filterIcon = "ion-chevron-down";
+        }
+    };
 };
 
-controllers.ProfielController = function($ionicScrollDelegate, AllData) {
+controllers.ProfielController = function ($ionicScrollDelegate, AllData) {
     var self = this;
-    
+
     $ionicScrollDelegate.scrollTop();
-    
+
     self.doelscreen = AllData.pref.doelscreen;
     self.textIcons = AllData.pref.textIcons;
     self.thema = "default";
 };
 
-controllers.DoelController = function($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
+controllers.DoelController = function ($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
     var self = this;
-    
+
     $ionicScrollDelegate.scrollTop();
     var doelID;
-    
-    StudentInfo.getDoel("test", $routeParams.doelID, function(results){
-        results.steps.forEach(function(object){
-            switch(object.done) {
+
+    StudentInfo.getDoel("test", $routeParams.doelID, function (results) {
+        results.steps.forEach(function (object) {
+            switch (object.done) {
                 case "good":
                     object.doneColor = "balanced";
                     break;
@@ -111,37 +122,38 @@ controllers.DoelController = function($ionicScrollDelegate, $routeParams, AllDat
                     object.doneColor = "positive";
             }
         });
-        
+
         self.doel = results;
         doelID = results.id;
     });
-    
-    self.toStap = function(stapID){
+
+    self.toStap = function (stapID) {
         AllData.toPage("Stap", "#/doelen/" + doelID + "/" + stapID);
     };
 };
 
-controllers.StapController = function($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
+controllers.StapController = function ($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
     var self = this;
-    
+
     $ionicScrollDelegate.scrollTop();
-    
-    StudentInfo.getStap("test", $routeParams.doelID, $routeParams.stapID, function(result){
-        switch(result.done) {
-                case "good":
-                    result.doneColor = "balanced";
-                    break;
-                case "bad":
-                    result.doneColor = "assertive";
-                    break;
-                default:
-                    result.doneColor = "positive";
-            }
+
+    StudentInfo.getStap("test", $routeParams.doelID, $routeParams.stapID, function (result) {
+        switch (result.done) {
+            case "good":
+                result.doneColor = "balanced";
+                break;
+            case "bad":
+                result.doneColor = "assertive";
+                break;
+            default:
+                result.doneColor = "positive";
+        }
         self.stap = result;
     });
 };
 
 zwengelControllers.controller('AllController', ['$ionicPlatform', '$window', 'AllData', controllers.AllController]);
+zwengelControllers.controller('LoginController', ['$ionicScrollDelegate', 'AllData', controllers.LoginController]);
 zwengelControllers.controller('DoelenController', ['$ionicScrollDelegate', 'AllData', 'StudentInfo', controllers.DoelenController]);
 zwengelControllers.controller('BeloningenController', ['$ionicScrollDelegate', 'StudentInfo', controllers.BeloningenController]);
 zwengelControllers.controller('ProfielController', ['$ionicScrollDelegate', 'AllData', controllers.ProfielController]);
