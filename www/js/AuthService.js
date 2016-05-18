719ef05a28afd3a27e9405dd1cdd0e8e18817857
BramHendriksDeveloper <bram5688@gmail.com>
2016-05-18 12:04:12 +0200

    consistentie

--- www/js/AuthService.js
+++ www/js/AuthService.js
@@ -1,35 +1,45 @@
-angular.module('authService', ['ngResource'])
+var authServices = angular.module('authServices', ['ngResource']);
 
-    .factory('AuthenticationService', function ($http, $rootScope) {
-        var service = {};
+var services_auth = {};
 
-        service.Login = function (username, password, callback) {
-            $http.post('https://zwengel-server.herokuapp.com/auth/login', { username: username, password: password })
-                .success(function (response) {
-                    callback(response);
-                });
-        };
+services_auth.Authentication = function($http, $rootScope) {
+    var self = this;
+    
+    self.Login = function (username, password, callback) {
+        $http.post('https://zwengel-server.herokuapp.com/auth/login', { username: username, password: password })
+            .success(function (response) {
+                callback(response);
+            });
+    };
+    
+    self.logout = function () {
+        localStorage.removeItem("token");
+        localStorage.removeItem("username");
+        $rootScope.loggedin = false;
+    }
+    
+    self.GetToken = function (callback) {
+        var token = localStorage.getItem('token');
+        callback(token);
+    }
+    
+    self.SetCredentials = function (response, input) {
+        localStorage.setItem('username', JSON.stringify(input.username));
 
-        service.logout = function () {
-            localStorage.removeItem("token");
-            localStorage.removeItem("username");
-            $rootScope.loggedin = false;
+        if (input.rememberme) {
+            localStorage.setItem('token', response.data.token);
+            console.log("set token through local storage");
         }
+        $rootScope.loggedin = true;
+        $rootScope.username = input.username;
+    };
+    
+    return {
+        Login: self.Login,
+        logout: self.logout,
+        GetToken: self.GetToken,
+        SetCredentials: self.SetCredentials
+    };
+};
 
-        service.GetToken = function (callback) {
-            var token = localStorage.getItem('token');
-            callback(token);
-        }
-
-        service.SetCredentials = function (response, input) {
-            localStorage.setItem('username', JSON.stringify(input.username));
-
-            if (input.rememberme) {
-                localStorage.setItem('token', response.data.token);
-                console.log("set token through local storage");
-            }
-            $rootScope.loggedin = true;
-            $rootScope.username = input.username;
-        };
-        return service;
-    });
+authServices.factory('Authentication', services_auth.Authentication);
