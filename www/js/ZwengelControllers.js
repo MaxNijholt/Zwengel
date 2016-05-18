48ba1294849e5b178dc7e6fce67e557bf02ad610
JoostVermeulen11 <jwaverme@avans.nl>
2016-05-18 12:16:15 +0200

    doelen read af zover mogelijk'

--- www/js/ZwengelControllers.js
+++ www/js/ZwengelControllers.js
@@ -152,26 +152,26 @@ controllers.DoelController = function ($ionicScrollDelegate, $routeParams, AllDa
     //self.doeletje = $routeParams.doelID;
 
     StudentInfo.getDoel("test", $routeParams.doelID, function (results) {
-        // results.steps.forEach(function (object) {
-        //     switch (object.done) {
-        //         case "good":
-        //             object.doneColor = "balanced";
-        //             break;
-        //         case "bad":
-        //             object.doneColor = "assertive";
-        //             break;
-        //         default:
-        //             object.doneColor = "positive";
-        //     }
-        // });
+        results.steps.forEach(function (object) {
+            switch (object.done) {
+                case "good":
+                    object.doneColor = "balanced";
+                    break;
+                case "bad":
+                    object.doneColor = "assertive";
+                    break;
+                default:
+                    object.doneColor = "positive";
+            }
+        });
 
         self.doel = results;
         doelID = results._id;
     });
 
-    // self.toStap = function (stapID) {
-    //     AllData.toPage("Stap", "#/doelen/" + doelID + "/" + stapID);
-    // };
+    self.toStap = function (stapID) {
+        AllData.toPage("Stap", "#/doelen/" + doelID + "/" + stapID);
+    };
 };
 
 controllers.StapController = function ($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
