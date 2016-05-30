7c39f1e9edba614d52892d50d122f6da15bcc505
BramHendriksDeveloper <bram5688@gmail.com>
2016-05-30 22:23:46 +0200

    step niet af: rood->blauw

--- www/js/ZwengelControllers.js
+++ www/js/ZwengelControllers.js
@@ -148,9 +148,6 @@ controllers.DoelController = function ($ionicScrollDelegate, $routeParams, AllDa
                 case true:
                     object.doneColor = "balanced";
                     break;
-                case false:
-                    object.doneColor = "assertive";
-                    break;
                 default:
                     object.doneColor = "positive";
             }
