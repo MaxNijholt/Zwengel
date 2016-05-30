de8c2ea2ca071fe7e740fff15f8aa899a3ac1793
Frank Kuipers <frank.k994@gmail.com>
2016-05-30 14:04:08 +0200

    Revert "Revert "Feature steps""
    This reverts commit c7d7b50a1fda266163d72599f3c1b019a52f985f.

--- www/js/DataServices.js
+++ www/js/DataServices.js
@@ -1,27 +1,34 @@
 var dataServices = angular.module('dataServices', ['ngResource']);
 
-var services_data = {};
-var dataCaller = {};
+var API = {
+    base: "http://zwengel-server.herokuapp.com",
+    login: "/auth/login",
+    goals: "/goals",
+    steps: "/steps"
+};
 
-services_data.StudentInfo = function(){
+var services_data = {};
+var dataCaller_data = {};
+
+services_data.StudentInfo = function ($http) {
     var self = this;
-    
-    self.getDoelen = function(studentID, onSucces){
-        dataCaller.getDoelen(studentID, onSucces);
+
+    self.getDoelen = function (studentID, onSucces) {
+        dataCaller_data.getDoelen($http, studentID, onSucces);
     };
-    
-    self.getDoel = function(studentID, doelID, onSucces){
-        dataCaller.getDoel(studentID, doelID, onSucces);
+
+    self.getDoel = function (studentID, doelID, onSucces) {
+        dataCaller_data.getDoel($http, studentID, doelID, onSucces);
     };
-    
-    self.getStap = function(studentID, doelID, stapID, onSucces){
-        dataCaller.getStap(studentID, doelID, stapID, onSucces);
+
+    self.getStap = function (studentID, doelID, stapID, onSucces) {
+        dataCaller_data.getStap($http, studentID, doelID, stapID, onSucces);
     };
-    
-    self.getBeloningen = function(studentID, onSucces){
-        dataCaller.getBeloningen(studentID, onSucces);
+
+    self.getBeloningen = function (studentID, onSucces) {
+        dataCaller_data.getBeloningen(studentID, onSucces);
     };
-    
+
     return {
         getDoelen: self.getDoelen,
         getDoel: self.getDoel,
@@ -30,355 +37,65 @@ services_data.StudentInfo = function(){
     };
 };
 
-dataCaller.testResult = [
-        {
-            "id": "t1",
-            "title": "Overtuigen",
-            "description": "De student kan met een medestudent in discussie gaan en de ander van eigen mening overtuigen.",
-            "icon": "ion-chatbubbles",
-            "done": "doing",
-            "motivation": null,
-            "startDate": "02-02-2016",
-            "endDate": "04-08-2016",
-            "steps": [
-                {
-                    "id": "st1",
-                    "title": "Vragen stellen",
-                    "description": "De student vraagt een medestudent om zijn mening.",
-                    "done": "good",
-                    "endDate": "02-02-2016"
-                },
-                {
-                    "id": "st2",
-                    "title": "Complimenten voor 5 weken",
-                    "description": "De student heeft 5 weken lang elke week een compliment aan een medestudent gegeven.",
-                    "done": "good",
-                    "endDate": "12-03-2015"
-                },
-                {
-                    "id": "st3",
-                    "title": "12 complimenten in 4 weken",
-                    "description": "De leerling heeft 4 weken lang elke week 3 complimenten gegeven aan medestudenten.",
-                    "done": "doing",
-                    "endDate": "29-05-2015"
-                },
-                {
-                    "id": "st4",
-                    "title": "21 complimenten in 3 weken",
-                    "description": "3 weken lang heeft de student elke dag een compliment gegeven aan een medestudent.",
-                    "done": "doing",
-                    "endDate": "14-07-2015"
-                }
-            ]
-        },
-        {
-            "id": "t2",
-            "title": "De rekentafels kennen",
-            "description": "De student kent de tafels van 1 tot en met 10 uit zijn hoofd.",
-            "icon": "ion-calculator",
-            "done": "doing",
-            "motivation": null,
-            "startDate": "01-01-2016",
-            "endDate": "04-05-2016",
-            "steps": [
-                {
-                    "id": "st1",
-                    "title": "Tafels 1, 2 en 3",
-                    "description": "De leerling moet de tafel van 1, 2 en 3 uit zijn hoofd kennen.",
-                    "done": "good",
-                    "endDate": "21-02-2016"
-                },
-                {
-                    "id": "st2",
-                    "title": "Tafels 4 en 5",
-                    "description": "De leerling moet de tafels van 1 tot en met 5 uit zijn hoofd kennen.",
-                    "done": "bad",
-                    "endDate": "02-03-2016"
-                },
-                {
-                    "id": "st3",
-                    "title": "Tafels 6 en 7",
-                    "description": "De leerling moet de tafels van 1 tot en met 7 uit zijn hoofd kennen.",
-                    "done": "doing",
-                    "endDate": "29-03-2016"
-                },
-                {
-                    "id": "st4",
-                    "title": "Tafels 8 en 9",
-                    "description": "Tafels 8 en 9 zijn erg moeilijk en moeten ook geleerd worden. De leerling moet de tafels van 1 tot en met 7 uit zijn hoofd kennen.",
-                    "done": "doing",
-                    "endDate": "21-04-2016"
-                },
-                {
-                    "id": "st5",
-                    "title": "Tafel van 10",
-                    "description": "10 is vrij makkelijk. Deze moet ook geleerd worden.",
-                    "done": "doing",
-                    "endDate": "04-05-2016"
-                }
-            ]
-        },
-        {
-            "id": "t3",
-            "title": "Het Wilhelmus kennen",
-            "description": "De student kent het Wilhelmus uit zijn hoofd.",
-            "icon": "ion-volume-high",
-            "done": "doing",
-            "motivation": null,
-            "startDate": "01-02-2016",
-            "endDate": "01-04-2016",
-            "steps": [
-                {
-                    "id": "st1",
-                    "title": "Eerste couplet",
-                    "description": "De leerling kent het eerste couplet uit zijn hoofd kennen.",
-                    "done": "good",
-                    "endDate": "15-02-2016"
-                },
-                {
-                    "id": "st2",
-                    "title": "Eerste refrein",
-                    "description": "De leerling kent het eerste refrein uit zijn hoofd kennen.",
-                    "done": "good",
-                    "endDate": "01-03-2016"
-                },
-                {
-                    "id": "st3",
-                    "title": "Tweede couplet",
-                    "description": "De leerling kent het tweede couplet uit zijn hoofd kennen.",
-                    "done": "good",
-                    "endDate": "11-03-2016"
-                },
-                {
-                    "id": "st4",
-                    "title": "Tweede refrein",
-                    "description": "De leerling kent het tweede refrein uit zijn hoofd kennen.",
-                    "done": "good",
-                    "endDate": "22-03-2016"
-                },
-                {
-                    "id": "st5",
-                    "title": "Derde couplet",
-                    "description": "De leerling kent het derde couplet uit zijn hoofd kennen.",
-                    "done": "doing",
-                    "endDate": "01-04-2016"
-                }
-            ]
-        },
-        {
-            "id": "t4",
-            "title": "Minder praten tijdens de les",
-            "description": "De student kan zich langere tijd stil houden in de les. Hierbij is het doel om 15 minuten lang niks te zeggen",
-            "icon": "ion-volume-low",
-            "done": "good",
-            "motivation": 4,
-            "startDate": "29-01-2016",
-            "endDate": "05-03-2016",
-            "steps": [
-                {
-                    "id": "st1",
-                    "title": "4x2 min",
-                    "description": "4 keer 2 min in de les niks zeggen",
-                    "done": "good",
-                    "endDate": "10-02-2016"
-                },
-                {
-                    "id": "st2",
-                    "title": "4x3 min",
-                    "description": "4 keer 3 min in de les niks zeggen.",
-                    "done": "good",
-                    "endDate": "12-02-2016"
-                },
-                {
-                    "id": "st3",
-                    "title": "2x5 min",
-                    "description": "2 keer moet in de les 5 minuten lang niks gezegd worden.",
-                    "done": "good",
-                    "endDate": "17-02-2016"
-                },
-                {
-                    "id": "st4",
-                    "title": "2x7 min",
-                    "description": "De student moet in de les 7 minuten lang niks zeggen. Dit moet tijdens een les 2 keer gedaan worden.",
-                    "done": "good",
-                    "endDate": "25-02-2016"
-                },
-                {
-                    "id": "st5",
-                    "title": "1x10 min",
-                    "description": "1 keer stil zijn 10 minuten lang.",
-                    "done": "good",
-                    "endDate": "29-02-2016"
-                },
-                {
-                    "id": "st6",
-                    "title": "1x15 min",
-                    "description": "De student moet een kwartier lang stil zijn tijdens de les.",
-                    "done": "good",
-                    "endDate": "05-03-2016"
-                }
-            ]
-        },
-        {
-            "id": "t5",
-            "title": "Planning nakomen",
-            "description": "De student kan de planning nakomen die hem aan het begin van de maand gegeven is, nakomen.",
-            "icon": "ion-calendar",
-            "done": "bad",
-            "motivation": 1,
-            "startDate": "01-07-2015",
-            "endDate": "31-01-2016",
-            "steps": [
-                {
-                    "id": "st1",
-                    "title": "4 van de 10 taken",
-                    "description": "Aan het eind van de maand moeten 4 van de 10 taken af zijn.",
-                    "done": "good",
-                    "endDate": "31-07-2015"
-                },
-                {
-                    "id": "st2",
-                    "title": "5 van de 10 taken",
-                    "description": "De helft van de taken moet aan het eind van de maand af zijn.",
-                    "done": "bad",
-                    "endDate": "31-08-2015"
-                },
-                {
-                    "id": "st3",
-                    "title": "6 van de 10 taken",
-                    "description": "6 taken moeten aan het eind van de maand af zijn.",
-                    "done": "bad",
-                    "endDate": "31-09-2015"
-                },
-                {
-                    "id": "st4",
-                    "title": "7 van de 10 taken",
-                    "description": "De student moet 7 taken van de 10 af hebben aan het einde van de maand.",
-                    "done": "bad",
-                    "endDate": "31-10-2015"
-                },
-                {
-                    "id": "st5",
-                    "title": "8 van de 10 taken",
-                    "description": "Aan het einde van de maand moeten 8 taken af zijn.",
-                    "done": "bad",
-                    "endDate": "31-11-2015"
-                },
-                {
-                    "id": "st6",
-                    "title": "9 van de 10 taken",
-                    "description": "9 van de 10 taken moeten zijn afgerond aan het einde van de maand.",
-                    "done": "bad",
-                    "endDate": "31-12-2015"
-                },
-                {
-                    "id": "st7",
-                    "title": "10 van de 10 taken",
-                    "description": "De student moet zijn planning volledig hebben nagekomen.",
-                    "done": "bad",
-                    "endDate": "31-01-2016"
-                }
-            ]
-        },
-        {
-            "id": "t6",
-            "title": "Complimenten geven",
-            "description": "De student moet dagelijks een compliment geven aan een medeleerling",
-            "icon": "ion-android-contacts",
-            "done": "good",
-            "motivation": 5,
-            "startDate": "19-11-2014",
-            "endDate": "10-05-2015",
-            "steps": [
-                {
-                    "id": "st1",
-                    "title": "Eerste compliment",
-                    "description": "De leerling heeft een medestudent een compliment gegeven.",
-                    "done": "good",
-                    "endDate": "31-12-2014"
-                },
-                {
-                    "id": "st2",
-                    "title": "Complimenten voor 5 weken",
-                    "description": "De student heeft 5 weken lang elke week een compliment aan een medestudent gegeven.",
-                    "done": "good",
-                    "endDate": "12-03-2015"
-                },
-                {
-                    "id": "st3",
-                    "title": "12 complimenten in 4 weken",
-                    "description": "De leerling heeft 4 weken lang elke week 3 complimenten gegeven aan medestudenten.",
-                    "done": "good",
-                    "endDate": "29-05-2015"
-                },
-                {
-                    "id": "st4",
-                    "title": "21 complimenten in 3 weken",
-                    "description": "3 weken lang heeft de student elke dag een compliment gegeven aan een medestudent.",
-                    "done": "good",
-                    "endDate": "14-07-2015"
-                }
-            ]
-        }
-    ];
-
-dataCaller.getDoelen = function(studentID, onSucces){    
-    onSucces(dataCaller.testResult);
+dataCaller_data.getDoelen = function ($http, studentID, onSucces, onFail) {
+    $http.get(API.base + API.goals)
+        .then(
+        function (results) {
+            onSucces(results.data.data);
+        }, function (response) {
+            console.log(response);
+        });
 };
 
-dataCaller.getDoel = function(studentID, doelID, onSucces){
+dataCaller_data.getDoel = function ($http, studentID, doelID, onSucces, onFail) {
     var doel = null;
-    
-    dataCaller.testResult.forEach(function(object){
-        if(object.id === doelID){
-            doel = object;
-        }
-    });
-    
-    onSucces(doel);
-};
 
-dataCaller.getStap = function(studentID, doelID, stapID, onSucces){
+    $http.get(API.base + API.goals + "/" + doelID)
+        .then(
+        function (results) {
+            onSucces(results.data);
+        }, function (response) {
+            onFail(response);
+        });
+}
+
+dataCaller_data.getStap = function ($http, studentID, doelID, stapID, onSucces) {
     var stap = null;
-    
-    dataCaller.testResult.forEach(function(object){
-        if(object.id === doelID){
-            object.steps.forEach(function(object2){
-               if(object2.id === stapID){
-                   stap = object2;
-               }
-            });
-        }
-    });
-    
-    onSucces(stap);
+
+    $http.get(API.base + API.goals + "/" + doelID + API.steps + "/" + stapID)
+        .then(
+        function (results) {
+            onSucces(results.data);
+        }, function (response) {
+            onFail(response);
+        });
 };
 
-dataCaller.getBeloningen = function(studentID, onSucces){
-    var testResult = [{ 
+dataCaller_data.getBeloningen = function (studentID, onSucces) {
+    var testResult = [{
         "title": "10 min. spelletje spelen in de les",
         "price": 5,
         "behaald": true
-    },{ 
-        "title": "15 min. spelletje spelen in de les",
-        "price": 7,
-        "behaald": false
-    },{ 
-        "title": "Thema Ridder",
-        "price": 25,
-        "behaald": false
-    },{ 
-        "title": "Thema Koning",
-        "price": 25,
-        "behaald": true
-    },{ 
-        "title": "Als eerste naar huis",
-        "price": 10,
-        "behaald": false
-    }
+    }, {
+            "title": "15 min. spelletje spelen in de les",
+            "price": 7,
+            "behaald": false
+        }, {
+            "title": "Thema Ridder",
+            "price": 25,
+            "behaald": false
+        }, {
+            "title": "Thema Koning",
+            "price": 25,
+            "behaald": true
+        }, {
+            "title": "Als eerste naar huis",
+            "price": 10,
+            "behaald": false
+        }
     ];
-    
+
     onSucces(testResult);
 };
 
-dataServices.factory('StudentInfo', services_data.StudentInfo);
+dataServices.factory('StudentInfo', ['$http', services_data.StudentInfo]);
--- www/js/ZwengelControllers.js
+++ www/js/ZwengelControllers.js
@@ -1,149 +1,224 @@
 var zwengelControllers = angular.module('zwengelControllers', []);
 var controllers = {};
 
-controllers.AllController = function($ionicPlatform, $window, AllData) {
+controllers.AllController = function ($ionicPlatform, $window, $route, AllData) {
     var self = this;
-    
+
     self.page = AllData.page;
-    
-    self.navDoelen = function(){
+
+    self.navDoelen = function () {
         AllData.toPage("Doelen", "#/doelen");
     };
-    
-    self.navBeloningen = function(){
+
+    self.navBeloningen = function () {
         AllData.toPage("Beloningen", "#/beloningen");
     };
-    
-    self.navProfiel = function(){
+
+    self.navProfiel = function () {
         AllData.toPage("Profiel", "#/profiel");
     };
-    
-    self.back = function(){
-		AllData.toBack();
+
+    self.back = function () {
+        AllData.toBack();
     };
-    
+
+    self.reload = function () {
+        $route.reload();
+    };
+
     $ionicPlatform.registerBackButtonAction(function () {
-		AllData.toBack(true);
-	}, 100);
+        AllData.toBack(true);
+    }, 100);
 };
 
-controllers.DoelenController = function($ionicScrollDelegate, AllData, StudentInfo) {
+controllers.LoginController = function ($ionicScrollDelegate, $rootScope, $ionicPopup, Authentication, AllData) {
     var self = this;
-    
     $ionicScrollDelegate.scrollTop();
-    
-    StudentInfo.getDoelen("test", function(results){
-        results.forEach(function(object){
-            switch(object.done) {
-                case "good":
+
+    if (localStorage.getItem("token") != null) {
+        AllData.toPage("Doelen", "#/doelen");
+    }
+    else {
+        AllData.toPage("Login", "#/login");
+    }
+
+    self.login = function (input) {
+        Authentication.Login(input.username, input.password, function (response) {
+            if (response.success) {
+                $ionicPopup.alert({
+                    title: 'Inloggen gelukt',
+                    template: 'Gelukt! U bent ingelogd en word nu doorgestuurd.'
+                })
+                    .then(function (res) {
+                        Authentication.SetCredentials(response, input);
+                        AllData.toPage("Doelen", "#/doelen");
+                        console.log("ingelogd!");
+                    });
+            }
+            else {
+                if (response.message === "invalid credentials")
+                    $ionicPopup.alert({
+                        title: 'Fout inloggen',
+                        template: 'Gebruikersnaam en/of wachtwoord onjuist.'
+                    });
+                else
+                    console.log(response.message);
+            }
+        })
+    }
+};
+
+controllers.DoelenController = function ($ionicScrollDelegate, AllData, StudentInfo) {
+    var self = this;
+
+    $ionicScrollDelegate.scrollTop();
+
+    StudentInfo.getDoelen("test", function (results) {
+
+        results.forEach(function (object) {
+            switch (object.state) {
+                case "doing":
+                    object.doneColor = "positive";
+                    break;
+                case "finished":
                     object.doneColor = "balanced";
                     break;
-                case "bad":
+                case "stopped":
                     object.doneColor = "assertive";
-                    break;
-                default:
-                    object.doneColor = "positive";
             }
         });
+
         self.targets = results;
+
     });
-    
-    self.toDoel = function(doelID){
+
+    self.toDoel = function (doelID) {
         AllData.toPage("Doel", "#/doelen/" + doelID);
     };
 };
 
-controllers.BeloningenController = function($ionicScrollDelegate, StudentInfo) {
+controllers.BeloningenController = function ($ionicScrollDelegate, StudentInfo) {
     var self = this;
-    
+
     $ionicScrollDelegate.scrollTop();
-    
+
     self.hideFilter = "ng-hide";
-	self.filterIcon = "ion-chevron-down";
-    StudentInfo.getBeloningen("test" , function(results){
+    self.filterIcon = "ion-chevron-down";
+    StudentInfo.getBeloningen("test", function (results) {
         self.rewards = results;
     });
-    	
-    self.buyReward = function(reward){
+
+    self.buyReward = function (reward) {
         var confirm = window.confirm("Koop beloning: " + reward.title);
         if (confirm == true) {
             alert("Gekocht");
         }
     };
-        
-	self.toggleHideFilter = function(bool){
-		if(self.hideFilter === "ng-hide" && !bool){
-			self.hideFilter = "ng-show";
-			self.filterIcon = "ion-chevron-up";
-		}else{
-			self.hideFilter = "ng-hide";
-			self.filterIcon = "ion-chevron-down";
-		}
-	};
+
+    self.toggleHideFilter = function (bool) {
+        if (self.hideFilter === "ng-hide" && !bool) {
+            self.hideFilter = "ng-show";
+            self.filterIcon = "ion-chevron-up";
+        } else {
+            self.hideFilter = "ng-hide";
+            self.filterIcon = "ion-chevron-down";
+        }
+    };
 };
 
-controllers.ProfielController = function($ionicScrollDelegate, AllData) {
+controllers.ProfielController = function ($ionicScrollDelegate, $ionicPopup, AllData, Authentication) {
     var self = this;
-    
+
     $ionicScrollDelegate.scrollTop();
-    
+
     self.doelscreen = AllData.pref.doelscreen;
     self.textIcons = AllData.pref.textIcons;
     self.thema = "default";
+
+    self.logout = function () {
+        Authentication.logout();
+        $ionicPopup.alert({
+            title: 'Uitloggen gelukt',
+            template: 'U bent uitgelogd en word nu doorgestuurd.'
+        }).then(function () {
+            AllData.toPage("Login", "#/login");
+        });
+    }
 };
 
-controllers.DoelController = function($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
+controllers.DoelController = function ($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
     var self = this;
-    
+
     $ionicScrollDelegate.scrollTop();
     var doelID;
-    
-    StudentInfo.getDoel("test", $routeParams.doelID, function(results){
-        results.steps.forEach(function(object){
-            switch(object.done) {
-                case "good":
+
+    StudentInfo.getDoel("test", $routeParams.doelID, function (results) {
+
+        var count = 0;
+
+        results.steps.forEach(function (object) {
+            if (object.completed)
+                count++;
+            switch (object.completed) {
+                case true:
                     object.doneColor = "balanced";
                     break;
-                case "bad":
+                case false:
                     object.doneColor = "assertive";
                     break;
                 default:
                     object.doneColor = "positive";
             }
         });
-        
-        self.doel = results;
-        doelID = results.id;
-    });
+
+        results.percentage = ((count / results.steps.length) * 100);
+       
+        if (results.percentage < 33){
+            results.progressStatus = "danger";
+            if(results.percentage <= 10)
+                results.percentage = 5;
+        }            
+        else if (results.percentage >= 33 && results.percentage < 66)
+            results.progressStatus = "warning";
+        else if (results.percentage > 66 && results.percentage <= 100)
+            results.progressStatus = "success";
     
-    self.toStap = function(stapID){
+        self.doel = results;
+        doelID = results._id;
+    });
+
+    self.toStap = function (stapID) {
         AllData.toPage("Stap", "#/doelen/" + doelID + "/" + stapID);
     };
 };
 
-controllers.StapController = function($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
+controllers.StapController = function ($ionicScrollDelegate, $routeParams, AllData, StudentInfo) {
     var self = this;
-    
+
     $ionicScrollDelegate.scrollTop();
-    
-    StudentInfo.getStap("test", $routeParams.doelID, $routeParams.stapID, function(result){
-        switch(result.done) {
-                case "good":
-                    result.doneColor = "balanced";
-                    break;
-                case "bad":
-                    result.doneColor = "assertive";
-                    break;
-                default:
-                    result.doneColor = "positive";
-            }
+
+    StudentInfo.getStap("test", $routeParams.doelID, $routeParams.stapID, function (result) {
+        switch (result.completed) {
+            case true:
+                result.doneColor = "balanced";
+                result.result = "Behaald";
+                break;
+            case false:
+                result.doneColor = "assertive";
+                result.result = "Niet behaald";
+                break;
+            default:
+                result.doneColor = "positive";
+                result.result = "Nog bezig";
+        }
         self.stap = result;
     });
 };
 
-zwengelControllers.controller('AllController', ['$ionicPlatform', '$window', 'AllData', controllers.AllController]);
+zwengelControllers.controller('AllController', ['$ionicPlatform', '$window', '$route', 'AllData', controllers.AllController]);
+zwengelControllers.controller('LoginController', ['$ionicScrollDelegate', '$rootScope', '$ionicPopup', 'Authentication', 'AllData', controllers.LoginController]);
 zwengelControllers.controller('DoelenController', ['$ionicScrollDelegate', 'AllData', 'StudentInfo', controllers.DoelenController]);
 zwengelControllers.controller('BeloningenController', ['$ionicScrollDelegate', 'StudentInfo', controllers.BeloningenController]);
-zwengelControllers.controller('ProfielController', ['$ionicScrollDelegate', 'AllData', controllers.ProfielController]);
+zwengelControllers.controller('ProfielController', ['$ionicScrollDelegate', '$ionicPopup', 'AllData', 'Authentication', controllers.ProfielController]);
 zwengelControllers.controller('DoelController', ['$ionicScrollDelegate', '$routeParams', 'AllData', 'StudentInfo', controllers.DoelController]);
 zwengelControllers.controller('StapController', ['$ionicScrollDelegate', '$routeParams', 'AllData', 'StudentInfo', controllers.StapController]);
