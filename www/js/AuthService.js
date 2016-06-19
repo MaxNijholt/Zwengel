d02175e5bf576a28ebaf912e517b42751c322821
Frank Kuipers <frank.k994@gmail.com>
2016-06-19 18:51:34 +0200

    localstorage fix

--- www/js/AuthService.js
+++ www/js/AuthService.js
@@ -3,25 +3,25 @@ var authServices = angular.module('authServices', ['ngResource']);
 var services_auth = {};
 var dataCaller_auth = {};
 
-services_auth.Authentication = function($http, $rootScope) {
+services_auth.Authentication = function ($http, $rootScope) {
     var self = this;
-    
+
     self.Login = function (username, password, onSucces) {
         dataCaller_auth.login($http, username, password, onSucces);
     };
-    
+
     self.logout = function () {
         localStorage.removeItem("token");
         localStorage.removeItem("username");
         $rootScope.loggedin = false;
     }
-    
+
     self.GetToken = function (callback) {
         var token = localStorage.getItem('token');
         callback(token);
     }
 
-    self.GetId = function() {
+    self.GetId = function () {
         return localStorage.getItem('id');
     };
 
@@ -29,18 +29,15 @@ services_auth.Authentication = function($http, $rootScope) {
         localStorage.removeItem("token");
 
         localStorage.setItem('username', JSON.stringify(input.username));
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('id', response.data.id);
 
-        if (input.rememberme) {
-            localStorage.setItem('token', response.data.token);
-            localStorage.setItem('id', response.data.id);
-        }
-        
         $rootScope.token = response.data.token;
         $rootScope.id = response.data.id;
         $rootScope.loggedin = true;
         $rootScope.username = input.username;
     };
-    
+
     return {
         Login: self.Login,
         logout: self.logout,
@@ -50,8 +47,8 @@ services_auth.Authentication = function($http, $rootScope) {
     };
 };
 
-dataCaller_auth.login = function($http, username, password, onSucces) {
-    $http.post(API.base + API.login, { username: username, password: password })
+dataCaller_auth.login = function ($http, username, password, onSucces) {
+    $http.post(API.base + API.login, {username: username, password: password})
         .success(
             function (response) {
                 onSucces(response);
