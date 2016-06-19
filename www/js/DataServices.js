c41550594fe9926e3c98d6a40e797a6e979f43d4
BramHendriksDeveloper <bram5688@gmail.com>
2016-06-19 18:48:09 +0200

    Revert "Revert "correct server""
    This reverts commit 514bac32b5a9bac383a2cb49de28a339fbd55e94.

--- www/js/DataServices.js
+++ www/js/DataServices.js
@@ -1,8 +1,7 @@
 var dataServices = angular.module('dataServices', ['ngResource']);
 
 var API = {
-    base: "http://localhost:5000",
-    // base: "http://kuipers.solutions:3000",
+    base: "http://kuipers.solutions:3000",
     login: "/auth/login",
     goals: "/goals",
     steps: "/steps",
